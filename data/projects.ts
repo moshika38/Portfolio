015c62a32773a5ba07a7fdbe97984e08bb82dc4f
moshika38 <moshika38@gmail.com>
2026-07-29 21:07:32 +0530

    update new project

--- data/projects.ts
+++ data/projects.ts
@@ -1,22 +1,19 @@
 export const projects = [
   {
+    title: "NodeX Cli / npm",
+    image: "/assets/projects/nodex.png",
+    description:
+      "A CLI tool to generate back end projects with preferred language, database, and stack configuration.",
+    link: "https://github.com/moshika38/NodeX-CLI",
+    tags: "#express #prisma #sqlite #mongoDB #postgreSQL #node.js",
+  },
+  {
     title: "Subhash Engineering",
     image: "/assets/projects/sb.jpg",
     description:
       "A Sri Lankan metal fabrication company specializing in custom welding and steel structures, based in Hali-Ela.",
     link: "https://www.subhashengineering.lk/",
     tags: "#web #lk #next.js #supabase #⁠postgreSql",
-    type: "web",
-  },
-
-  {
-    title: "CMC UAE",
-    image: "/assets/projects/cmc.png",
-    description:
-      "CMC UAE is a responsive app that keeps users updated with temple activities, real-time alerts, and upcoming events directly on their devices.",
-    link: "https://play.google.com/store/apps/details?id=com.meditationcenter.uae",
-    tags: "#firebase #flutter #dart #android #ios #push-notification",
-    type: "mobile",
   },
 
   {
@@ -26,7 +23,14 @@ export const projects = [
       "A fully offline point-of-sale (POS) and billing system built with Flutter for B Vibe Cafe.",
     link: "https://github.com/moshika38/B-Vibe-Cafe",
     tags: "#pos #sqlitedb #flutter #desktop #inventory #billing",
-    type: "web",
+  },
+  {
+    title: "CMC UAE",
+    image: "/assets/projects/cmc.png",
+    description:
+      "CMC UAE is a responsive app that keeps users updated with temple activities, real-time alerts, and upcoming events directly on their devices.",
+    link: "https://play.google.com/store/apps/details?id=com.meditationcenter.uae",
+    tags: "#firebase #flutter #dart #android #ios #push-notification",
   },
   {
     title: "GourmetHaven",
@@ -35,7 +39,6 @@ export const projects = [
       "A modern, responsive restaurant website built with Next.js, featuring menus, location details, payments, and authentication.",
     link: "https://gourmet-haven-pied.vercel.app/",
     tags: "#firebase #stripe #next.js #mongodb #typescript",
-    type: "web",
   },
   {
     title: "CoffeeShop",
@@ -44,7 +47,6 @@ export const projects = [
       "Front-end code for the Coffee House website, designed to showcase its menu, brand, and offerings while engaging potential customers through a modern, responsive interface.",
     link: "https://github.com/moshika38/coffeeShop",
     tags: "#html #css #js #bootstrap",
-    type: "web",
   },
   {
     title: "Social Connect",
@@ -53,7 +55,6 @@ export const projects = [
       "A feature-rich social media app built with Flutter, providing users with an engaging platform to connect, share, and communicate effortlessly.",
     link: "https://github.com/moshika38/Flutter-Social-Media-App",
     tags: "#flutter #dart #android #ios #firebase #push-notification",
-    type: "mobile",
   },
 
   {
@@ -63,7 +64,6 @@ export const projects = [
       "A Flutter-based task management app that helps users organize and schedule tasks with an intuitive interface. It includes attractive animated activities and provides scheduled notifications.",
     link: "https://github.com/moshika38/Work-Manager-App",
     tags: "#android #ios #Scheduling #CloudFirestone #AlertNotifications",
-    type: "mobile",
   },
 
   {
@@ -73,7 +73,6 @@ export const projects = [
       "An advanced AI-powered chat application built with Flutter. This app enables users to interact with AI for personalized conversations, providing an engaging and intelligent chat experience.",
     link: "https://github.com/moshika38/Ai-Chat-App-with-Flutter",
     tags: "#android #ios #Authentication #Ai #Chat #Gemini",
-    type: "mobile",
   },
 
   {
@@ -83,7 +82,6 @@ export const projects = [
       "A Flutter-based application designed for efficient document scanning and management. The app includes both free and premium features, with a seamless payment gateway integration for upgrading to the premium version. The premium version offers additional capabilities tailored for professional use.",
     link: "https://github.com/moshika38/Scanner-App",
     tags: "#android #ios #Stripe #API #Payment #Gateway",
-    type: "mobile",
   },
   {
     title: "Farm Management App UI/UX",
@@ -92,7 +90,6 @@ export const projects = [
       "A modern farm management mobile app UI/UX designed in Flutter, focusing on clean layouts, smooth user flow, and ease of use. Includes authentication screens, dashboard, tasks, chat, and weather overview. Backend integration not included — UI/UX only.",
     link: "https://github.com/moshika38/Farm-App-UI",
     tags: "#android #ios #Flutter #UI/UX",
-    type: "mobile",
   },
   {
     title: "Drone Controller App",
@@ -101,7 +98,6 @@ export const projects = [
       "Developed a full-stack mobile app to control drones via Arduino with Bluetooth communication. Designed the UI using Flutter, integrated flight control, real-time telemetry, and camera management.",
     link: "https://github.com/moshika38/drone-controller-app",
     tags: "#android #ios #Arduino #Bluetooth #Flutter",
-    type: "mobile",
   },
 
   {
@@ -111,6 +107,5 @@ export const projects = [
       "An e-commerce mobile app built with Flutter for a coffee shop, using Firebase for authentication and Firestore for real-time data storage.",
     link: "https://github.com/moshika38/E-Commers-App",
     tags: "#flutter #dart #android #ios #firebase #firebase-auth #cart",
-    type: "mobile",
   },
 ];
